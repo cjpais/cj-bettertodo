7f63a1581c2a9ede92093e254724bcec48b97afb
sipjca <cjpais@ucsd.edu>
2016-07-24 13:04:16 -0700

    basic additions

--- app.py
+++ app.py
@@ -1,15 +1,80 @@
-from flask import Flask, render_template
-from flask.ext.login import LoginManager
-from app_model import db
+from flask import Flask, render_template, redirect, flash, request, session, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
-login_manager = LoginManager()
-login_manager.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/todo.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column('user_id', db.Integer, primary_key = True)
+    username = db.Column('username', db.String(40), unique=True)
+    email = db.Column('email', db.String(120), unique=True)
+    password = db.Column('password', db.String(30))
+    created = db.Column('created', db.DateTime)
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def __init__(self, username, email, password):
+		self.username = username
+		self.password = generate_password_hash(password)
+		self.email = email
+		self.created = datetime.utcnow()
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+	def __repr__(self):
+		return '<User %r>' % self.username
 
 @app.route('/')
 def index():
+    db.create_all()
+    print "redirected"
+    print session
+    if not session.get('logged_in'):
+        return render_template('login_template.html')
+    else:
+        return "meantime before index homepage thing"
+
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    print request.method
+    #Session = sessionmaker(bind=app.config["SQLALCHEMY_DATABASE_URI"])
+    #s = Session()
+    if request.method == 'POST':
+        inputUser = str(request.form['inputUsername'])
+        inputPass = str(request.form['inputPassword'])
+        result = User.query.filter_by(username=inputUser).first()
+        valid = result and result.check_password(inputPass)
+        if valid:
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+        else:
+            flash("WRONG PASSWORD")
+            return render_template('login_template.html')
     return render_template('login_template.html')
 
+@app.route('/register/', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        user = request.form['inputUsername']
+        email = request.form['inputEmail']
+        passw = request.form['inputPassword']
+        newUser = User(user, email, passw)
+        db.session.add(newUser)
+        db.session.commit()
+        users = User.query.all()
+        return redirect(url_for('login'))
+    return render_template('register_template.html')
+
+@app.route('/logout/')
+def logout():
+    session['logged_in'] = False
+    return home()
+
 if __name__ == '__main__':
-    app.secret_key = "CHANGE THIS QUICKLY"
+    app.secret_key = os.urandom(80)
     app.run(debug=True, port=8080)
--- app_model.py
+++ app_model.py
@@ -6,26 +6,31 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/todo.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
-	id = db.Column('user_id', db.Integer, primary_key = True)
-	username = db.Column('username', db.String(40), unique=True)
-	password = db.Column('password', db.String(30))
-	email = db.Column('email', db.String(120), unique=True)
-	created = db.Column('created', db.DateTime)
+    __tablename__ = 'user'
+    id = db.Column('user_id', db.Integer, primary_key = True)
+    username = db.Column('username', db.String(40), unique=True)
+    email = db.Column('email', db.String(120), unique=True)
+    password = db.Column('password', db.String(30))
+    created = db.Column('created', db.DateTime)
+    authenticated = db.Column(db.Boolean, default=False)
 
-	def __init__(self, username, password, email):
+    def __init__(self, username, password, email):
 		self.username = username
 		self.password = password
 		self.email = email
 		self.created = datetime.utcnow()
 
-	def get_auth(self):
+    def is_active(self):
 		return True
 
-	def get_active(self):
-		return True
-
-	def get_id(self):
+    def get_id(self):
 		return unicode(self.id)
+
+    def is_authenticated(self):
+		return self.authenticated
+
+    def is_anonymous(self):
+        return False
 
 	def __repr__(self):
 		return '<User %r>' % self.username
